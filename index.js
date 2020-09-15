124752177cc8a7e37ed6f6fe6cd9532ba3b047b4
raghadanees <raghad.anees@hotmail.com>
2020-09-15 07:27:49 +0300

    initial lab7

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// I will require the server file and start the server here.
+const serverModule = require('./lib/server.js');
+
+// run my application
+serverModule.start();
+
+// node index.js
