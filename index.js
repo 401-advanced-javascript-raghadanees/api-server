d80218f5ce49232cf7aa37e26f5c3e64b9722768
raghadanees <raghad.anees@hotmail.com>
2020-09-16 08:15:21 +0300

    initial

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 // I will require the server file and start the server here.
 const serverModule = require('./lib/server.js');
 
+// require mongoose and connect to db
+const mongoose = require('mongoose');
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+mongoose.connect(MONGODB_URL, mongooseOptions);
+
 // run my application
 serverModule.start();
 
 // node index.js
+// node .
